2ca3056983179cbca7f826c6397f563eae62aea5
yanguadotdev <yanguasamir05@gmail.com>
2025-05-29 14:34:22 -0500

    feat: enhance video animation and add text overlay styles for improved user experience

--- src/app.js
+++ src/app.js
@@ -8,9 +8,18 @@ main.addEventListener('scroll', () => {
   animateVideo()
 })
 
+const headerLeft = $('.text__header__left')
+const headerRight = $('.text__header__right')
+
 function animateVideo() {
   let { bottom } = videoSection.getBoundingClientRect()
   let scale = 1 - (bottom - window.innerHeight) * 0.0005
   scale = scale < 0.2 ? 0.2 : scale > 1 ? 1 : scale
   video.style.transform = `scale(${scale})`
+
+  // Text transformation
+  let textTrans = bottom - window.innerHeight
+  textTrans = textTrans < 0 ? 0 : textTrans
+  headerLeft.style.transform = `translate(${-textTrans}px)`
+  headerRight.style.transform = `translate(${textTrans}px)`
 }
