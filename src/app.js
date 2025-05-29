6cfca9db33c4e80d98e609195b8f5316d73e537e
yanguadotdev <yanguasamir05@gmail.com>
2025-05-29 14:12:07 -0500

    feat: restructure HTML sections and enhance CSS for improved layout and scrolling effects

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 import { lerp, $ } from './utils.js'
 
+const main = $('main')
 const video = $('video')
 const videoSection = $('#video')
 
-window.addEventListener('scroll', () => {
+main.addEventListener('scroll', () => {
   animateVideo()
 })
 
 function animateVideo() {
   let { bottom } = videoSection.getBoundingClientRect()
-  let scale = 1 - (bottom - window.innerHeight) * .0005
-  scale = scale < .2 ? .2 : scale > 1 ? 1 : scale
+  let scale = 1 - (bottom - window.innerHeight) * 0.0005
+  scale = scale < 0.2 ? 0.2 : scale > 1 ? 1 : scale
   video.style.transform = `scale(${scale})`
 }
